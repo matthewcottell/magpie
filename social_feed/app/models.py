cd191a062bc1862a37f4f2e7f8dd9d895b03f418
Matthew Cottell <matthew.cottell@cottonteam.com>
2025-03-14 09:57:16 -0500

    ruff files

--- social_feed/app/models.py
+++ social_feed/app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 class User(models.Model):
     username = models.CharField(max_length=100)
     email = models.EmailField()
 
     def __str__(self):
         return self.username
+
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -16,8 +18,12 @@ class Post(models.Model):
         return f"Post by {self.user.username}"
 
     class Meta:
-        ordering = ['-created_at']  # To order posts by creation time
+        ordering = ["-created_at"]  # To order posts by creation time
+
+
 class Friendship(models.Model):
-    user = models.ForeignKey(User, related_name='friend', on_delete=models.CASCADE)
-    friend = models.ForeignKey(User, related_name='friends_with', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="friend", on_delete=models.CASCADE)
+    friend = models.ForeignKey(
+        User, related_name="friends_with", on_delete=models.CASCADE
+    )
     created_at = models.DateTimeField(auto_now_add=True)
--- social_feed/app/views.py
+++ social_feed/app/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
 from .models import Post
 
+
 def home(request):
     posts = Post.objects.all()  # Fetch all posts (you can add filtering for friends)
-    return render(request, 'index.html', {'posts': posts})
+    return render(request, "index.html", {"posts": posts})
+
 
 from django.http import JsonResponse
 from .models import Post
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 
+
 @csrf_exempt
 def create_post(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         user = request.user  # You can get the logged-in user here
-        content = request.POST.get('content')
-        post = Post.objects.create(user=user, content=content, created_at=timezone.now())
-        return JsonResponse({'post_id': post.id, 'content': post.content})
+        content = request.POST.get("content")
+        post = Post.objects.create(
+            user=user, content=content, created_at=timezone.now()
+        )
+        return JsonResponse({"post_id": post.id, "content": post.content})
